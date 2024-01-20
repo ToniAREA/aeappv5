cd601a9d61a2da61c1cd1d4e4c98e073af13f4f7
ToniAREA <43147720L@gmail.com>
2024-01-20 11:49:59 +0100

    Commit before merge CUSTOM into PRODUCTION branch done by BASH script

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,23 +1,48 @@
 @extends('layouts.frontend')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <!-- Principal div: 12 columnas en desktop y 10 en pantallas más pequeñas -->
+            <div class="col-lg-12 col-md-10">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body"> <!-- Add text-center class here -->
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="row">
+                            @foreach ($wlists as $wlist)
+                                <!-- Tarjetas: 4 columnas en desktop, 2 en tablet y 1 en móvil -->
+                                <div class="col-lg-4 col-md-6 col-sm-12">
+                                    <div class="card m-1">
+                                        <div class="card-header">
+                                            <a href="">{{ $wlist->name }}</a>
+                                        </div>
+                                        <div class="card-body">
+                                            <p>{{ $wlist->description }}</p>
+                                            <p>{{ $wlist->created_at }}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                            <!-- Agrega los enlaces de paginación al final de la lista -->
+                            <div class="pagination-wrapper flex justify-center">
+                                <nav class="flex" aria-label="Pagination Navigation">
+                                    <!-- Contenido de paginación aquí -->
+                                    {{ $wlists->links() }}
+                                    
+                                </nav>
+                            </div>
+
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
