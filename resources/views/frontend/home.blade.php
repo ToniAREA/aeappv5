631555a75cdf5a815f83df841adf7922c739436b
ToniAREA <43147720L@gmail.com>
2024-01-22 11:14:32 +0100

    Commit before merge CUSTOM into PRODUCTION branch done by BASH script

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,21 +14,41 @@
 
                 <div class="row">
                     @foreach ($wlists as $wlist)
-                        <!-- Tarjetas: 4 columnas en desktop, 2 en tablet y 1 en móvil -->
-                        <div class="col-lg-4 col-md-6 col-sm-12">
-                            <div class="card m-1">
-                                <div class="card-header">
-                                    <a href="">{{ $wlist->client['name'] ?? '-' }}, {{ $wlist->client['lastname'] ?? '-' }}</a><br>
-                                    <span class="text-muted">({{ $wlist->created_at->diffInDays() }} days ago)</span>
+                        <div class="col-12 col-md-6 col-lg- mt-2">
+                            <div class="card custom-card">
+                                <div class="card-body p-3 d-flex flex-row position-relative">
+                                    <!-- ID Badge -->
+                                    <div class="col-2 d-flex align-items-center justify-content-center custom-badge">
+                                        <span>{{ $wlist->id }}</span>
+                                    </div>
 
-                                </div>
-                                <div class="card-body">
-                                    <p>{{ $wlist->description }}</p>
-                                    <p>{{ $wlist->created_at }}</p>
+                                    <!-- Boat Name and Description -->
+                                    <div class="col-8 text-container">
+                                        <a href="/boats/{{ $wlist->boat['id'] }}" class="custom-link d-block mb-1">
+                                            <h2 class="h6 mb-0 custom-title">{{ $wlist->boat_namecomplete }}</h2>
+                                        </a>
+                                        <a href="/wlists/{{ $wlist->id }}" class="custom-description-link">
+                                            <p class="custom-description mb-0">
+                                                {{ Str::limit($wlist->description, 50, '...') }}
+                                            </p>
+                                        </a>
+                                    </div>
+
+                                    <!-- Elapsed Time in Days -->
+                                    <div
+                                        class="col-2 d-flex flex-column align-items-center justify-content-start custom-elapsed-time">
+                                        @php
+                                            $elapsedDays = \Carbon\Carbon::now()->diffInDays($wlist->created_at);
+                                        @endphp
+                                        <span>{{ $elapsedDays }} days</span>
+                                        <span>{{ strtoupper($wlist->type) }}</span>
+                                        <span>{{ strtoupper($wlist->status) }}</span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     @endforeach
+
                     <!-- Agrega los enlaces de paginación al final de la lista -->
                     <div class="pagination-wrapper flex justify-center m-3">
                         <nav class="flex" aria-label="Pagination Navigation">
