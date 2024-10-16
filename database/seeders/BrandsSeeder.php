1b4bf24ec7ce399995531cff6ffd0930367787ad
ToniAREA <75792361+ToniAREA@users.noreply.github.com>
2024-10-16 21:59:08 +0200

    fixing BrandsSeeder.php

--- database/seeders/BrandsSeeder.php
+++ database/seeders/BrandsSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Brand;
 use Illuminate\Support\Str;
 
-class BrandSeeder extends Seeder
+class BrandsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
